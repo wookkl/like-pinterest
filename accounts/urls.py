f1b2fa9c9b758af20506825880af9042c858118b
wookkl <wjddnr315@gmail.com>
2020-12-21 01:32:57 +0900

    delete view

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("logout/", views.AccountLogoutView.as_view(), name="logout"),
     path("<int:pk>/", views.AccountDetailView.as_view(), name="detail"),
     path("update/<int:pk>/", views.AccountUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", views.AccountDeleteView.as_view(), name="delete"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 # Django
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse, reverse_lazy
@@ -10,6 +10,9 @@ from . import forms
 
 
 class AccountCreateView(CreateView):
+
+    """ Account Create View Definition """
+
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy("core:home")
@@ -17,21 +20,42 @@ class AccountCreateView(CreateView):
 
 
 class AccountLoginView(LoginView):
+
+    """ Account Log in View Definition """
+
     template_name = "accounts/login.html"
 
 
 class AccountLogoutView(LogoutView):
+
+    """ Account Log out View Definition """
+
     pass
 
 
 class AccountDetailView(DetailView):
+
+    """ Account Detail View Definition """
+
     model = User
     template_name = "accounts/detail.html"
     context_object_name = "target_user"
 
 
 class AccountUpdateView(UpdateView):
+
+    """ Account Update View Definition """
+
     model = User
     form_class = forms.AccountUpdateForm
     success_url = reverse_lazy("account:detail")
     template_name = "accounts/update.html"
+
+
+class AccountDeleteView(DeleteView):
+
+    """ Account Delete View Definition """
+
+    model = User
+    success_url = reverse_lazy("accounts:login")
+    template_name = "accounts/delete.html"
